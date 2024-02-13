09c3c5744a8401fdd05f337dca8e11c6a56aaaa7
Xamilio <119781901+Xamilio@users.noreply.github.com>
2024-02-13 22:14:39 +0200

    Update Homework51.cpp

--- Homework51/Homework51.cpp
+++ Homework51/Homework51.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+#include <iostream>
+#include <cmath> 
 using namespace std;
 template<typename T>
 class DynArray
@@ -19,7 +20,7 @@ public:
         {
             arr[i] = object.arr[i];
         };
-         cout << "DynArr copy constructed for " << size << " elements, for " << this<< '\n';
+        cout << "DynArr copy constructed for " << size << " elements, for " << this << '\n';
     }
     ~DynArray()
     {
@@ -28,12 +29,20 @@ public:
         cout << "DynArr destructed for " << size << " elements, for " << this << '\n';
     }
 
-    T getElem(int idx) { return arr[idx]; }
+    T getElem(int idx)
+    {
+        return arr[idx];
+    }
 
-    void setElem(int idx, T val) { arr[idx] = val; }
+    void setElem(int idx, T val)
+    {
+        arr[idx] = val;
+    }
+
+
     void print()
     {
-        for (int i{ 0 }; i < size; ++i)
+        for (int i = 0; i < size; ++i)
         {
             cout << arr[i] << ' ';
         }
@@ -41,7 +50,7 @@ public:
     }
     void randomize()
     {
-        for (int i{ 0 }; i < size; ++i)
+        for (int i = 0; i < size; ++i)
         {
             arr[i] = static_cast<T>(rand() % 10);
         }
@@ -51,25 +60,59 @@ public:
         T sum = 0;
         for (int i = 0; i < size; ++i)
         {
-            sum += arr[i];
+            sum = sum + arr[i];
         }
         return sum;
     }
 };
-class Point 
+class Point
 {
+public:
     int x;
     int y;
+
+
+    Point() : x(0), y(0) {}
+
+    Point(int l)
+    {
+        x = l;
+        y = l;
+    }
+
+    Point(int xVal, int yVal) : x(xVal), y(yVal) {}
+
+    Point& operator =(int& a)
+    {
+        x = a;
+        y = a;
+        return *this;
+    }
+    operator int() const
+    {
+        return static_cast<int>(sqrt(x * x + y * y));
+    }
 };
 
 int main()
 {
-    DynArray<int> intArray(5);
-    DynArray<double> doubleArray(5);
-    DynArray<char> charArray(5);
-    DynArray<Point> pointArray(5);
-
+    DynArray<int> intArray(5);  
     intArray.randomize();
+    intArray.print();
+    cout << "Сумма: " << intArray.sum() << "\n";
+    
+    DynArray<double> doubleArray(5);
     doubleArray.randomize();
+    doubleArray.print();
+    cout << "Сумма: " << doubleArray.sum() << "\n";
+    
+    DynArray<char> charArray(5);
     charArray.randomize();
+    charArray.print();
+    cout << "Сумма: " << charArray.sum() << "\n";
+
+    DynArray<Point> pointArray(5);
+    pointArray.randomize();
+    pointArray.print();
+    cout << "Сумма: " << pointArray.sum() << "\n";
 }
